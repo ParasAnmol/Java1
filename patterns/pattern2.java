089b65e0eb2a867ae8891bf38dc89213758e2786
ParasAnmol <parasanmol16@gmail.com>
2022-04-07 10:22:18 -0400

    new start

---
+++ patterns/pattern2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        // write ur code here
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i ; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+                                or
+
+                                import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int  n = scn.nextInt();
+        for (int  i = 1 ; i <= n ; i++){
+            for(int  j = n ; j >= i ; j--){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
